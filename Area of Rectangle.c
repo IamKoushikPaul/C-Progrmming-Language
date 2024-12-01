f8502c754bb6f6abc76708c39aee0e7e650cdbec
Koushik PauL <koushik2005paul@gmail.com>
2024-12-01 14:23:34 +0600

    Add files via upload

---
+++ Area of Rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int langth,width,area;
+    printf("Enter Langth=");
+    scanf("%d",&langth);
+    printf("Enter Width=");
+    scanf("%d",&width);
+    area = langth*width;
+    printf("Area of Rectangle=%d",area);
+
+    return 0;
+
+
+}
---
+++ Assignment operators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    //a+=2;
+    //a-=2;
+   //a/=2;
+    //a*=2;
+    //a%=2;
+
+    printf("%d",a);
+
+    return 0;
+}
