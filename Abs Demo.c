ef61b286bd3d3130cc99566a8b7db1ecea24200a
Koushik PauL <koushik2005paul@gmail.com>
2024-12-18 21:38:16 +0600

    Add files via upload

---
+++ Abs Demo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    /*int result = abs(-25);
+    printf("%d",result);*/
+
+    int num1=10,num2=11;
+    printf("Enter Number 1:%d\n",num1);
+           //scanf("%d",&num1);
+           printf("Enter Number 2:%d",num2);
+                 // scanf("%d",&num2);
+
+
+
+                  return 0;
+}
